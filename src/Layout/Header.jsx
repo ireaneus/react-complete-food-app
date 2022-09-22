3b919f41d9cbd26897d6abc3f31ead8ac4302635
ireaneus <bwdave@protonmail.com>
2022-09-22 16:01:02 -0600

    Food app components complete without cart modal

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,16 +1,19 @@
 import React, { Fragment } from 'react';
 
 import styles from './Header.module.css';
-// import mealsImg from 'https://github.com/academind/react-complete-guide-code/blob/11-practice-food-order-app/extra-files/meals.jpg';
+import mealsImg from '../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
-      <div className={styles['main-image']}></div>
+      <div className={styles['main-image']}>
+        <img src={mealsImg} alt='Good food' />
+      </div>
     </Fragment>
   );
 };
