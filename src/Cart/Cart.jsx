1943a5ecd4a076b9ac22649c5f10c29d36801377
Ireaneus <bwdave@protonmail.com>
2022-09-25 20:28:39 +0000

    Update 6 files

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import Modal from '../UI/Modal';
-import styles from './Cart.module.css';
+import React from "react";
+import Modal from "../UI/Modal";
+import styles from "./Cart.module.css";
 
 const Cart = (props) => {
-
-    const cartItems = <ul className={styles['cart-items']}>{[{ id: 'c1', name: 'Chilito', amount: 3, price: 2.17}, ].map((item) => <li>{item.name}</li>)}</ul>;
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {[{ id: "c1", name: "Chilito", amount: 3, price: 2.17 }].map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
 
   return (
-    <Modal>
-        {cartItems}
-        <div className={styles.total}>
-            <span>Total Amount</span>
-            <span>6.91</span>
-        </div>
-        <div className={styles.actions}>
-            <button className={styles['button--alt']}>Close</button>
-            <button className={styles.button}>Order</button>
-        </div>
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>6.91</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        <button className={styles.button}>Order</button>
+      </div>
     </Modal>
   );
 };
